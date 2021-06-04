c61be23c12dc211ad4f4b9a82e1a28ea458afed0
Ken Goldfarb <hello@kengoldfarb.com>
2021-06-04 15:16:18 -0400

    lint config cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,5 @@
 module.exports = {
 	extends: 'kengoldfarb',
-	globals: {
-		lthr: true,
-		log: true,
-		describe: true,
-		before: true,
-		beforeEach: true,
-		afterEach: true,
-		after: true,
-		it: true,
-		config: true,
-		orm: true,
-		Promise: true,
-	},
-	rules: {
-		"no-param-reassign": ["error", { "props": false }],
-		"react/static-property-placement": 0
-	}
+	globals: {},
+	rules: {}
 }
